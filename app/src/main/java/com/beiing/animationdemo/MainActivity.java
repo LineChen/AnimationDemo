88e4124a9fd4649057a9a206ddd93fc7c686dad5
Beiing <15764230067@163.com>
2016-11-18 14:25:39 +0800

    first commit

---
+++ app/src/main/java/com/beiing/animationdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.beiing.animationdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Test1Activity(View view) {
+        startActivity(new Intent(this, Test1Activity.class));
+    }
+}
